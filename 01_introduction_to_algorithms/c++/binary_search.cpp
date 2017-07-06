f493ff8136ba483a26b2f8cc38ac787b78ec58fe
Terrence Young <tyoungjr2005@gmail.com>
2017-07-05 22:27:11 -0500

    Chapter C++ binary search implementaton

---
+++ 01_introduction_to_algorithms/c++/binary_search.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+int integer_binary_search(int list[],int item, int lenghtOfArray);
+
+int main(int argc, char **argv) 
+{
+    
+    int list[5] = { 11, 34, 78, 101, 120 };
+    char item = NULL;
+	
+	int size = ((sizeof list) / (sizeof list[0]));
+
+#ifdef DEBUG
+	std::cout << "length of array" << size << std::endl;
+#endif // DEBUG
+    
+	int result = integer_binary_search(list, item, size);
+	if (result == -1)
+	{
+		std::cout << "An error has occured please restart." << std::endl;
+		return result;
+	} 
+	
+	if (result == 0)
+	{
+		std::cout << "Item not found please search again" << std::endl;
+	}
+	if (item == list[result])
+	{
+		std::cout << item << " was found at " << result << " in the list" << std::endl;
+	}
+
+    return 0;
+}
+
+int integer_binary_search( int list[], int item, int size_of_array)
+{
+	if (list == NULL || item == NULL || size_of_array == 0)
+		return -1;
+
+	int low = 0;
+	int mid;
+	int high; 
+	int guess;
+	high = size_of_array -1;
+	 
+	while (low <= high)
+	{
+		mid = low + high;
+		guess = list[mid];
+		if (guess == item)
+		{
+			return mid;
+		}
+		if (guess > item)
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+
+	}
+
+    return 0; 
+
+}
